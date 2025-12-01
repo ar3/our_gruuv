7ca98ccb2e4b97c11e303ecad6c14c8e95f265c6
AR3 <ar3@ar3.me>
2025-12-01 11:03:27 -0500

    small adjustments for specs

--- spec/requests/organizations/check_ins_security_spec.rb
+++ spec/requests/organizations/check_ins_security_spec.rb
@@ -15,12 +15,16 @@ RSpec.describe 'Self-Check-In Security', type: :request do
   before do
     # Create active employment for the person
     create(:employment_tenure, teammate: person_teammate, company: organization, started_at: 1.year.ago, ended_at: nil)
+    person_teammate.update!(first_employed_at: 1.year.ago)
     # Create active employment for manager
     create(:employment_tenure, teammate: manager_teammate, company: organization, started_at: 1.year.ago, ended_at: nil)
+    manager_teammate.update!(first_employed_at: 1.year.ago)
     # Create active employment for employment manager
     create(:employment_tenure, teammate: employment_manager_teammate, company: organization, started_at: 1.year.ago, ended_at: nil)
+    employment_manager_teammate.update!(first_employed_at: 1.year.ago)
     # Create active employment for regular teammate
     create(:employment_tenure, teammate: regular_teammate, company: organization, started_at: 1.year.ago, ended_at: nil)
+    regular_teammate.update!(first_employed_at: 1.year.ago)
     # Set manager relationship
     person_teammate.employment_tenures.first.update!(manager: manager)
   end
@@ -85,13 +89,15 @@ RSpec.describe 'Self-Check-In Security', type: :request do
 
       before do
         create(:employment_tenure, teammate: other_org_teammate, company: other_organization, started_at: 1.year.ago, ended_at: nil)
+        other_org_teammate.update!(first_employed_at: 1.year.ago)
         sign_in_as_teammate_for_request(other_org_person, other_organization)
       end
 
       it 'denies access' do
         get organization_person_check_ins_path(organization, person)
         expect(response).to have_http_status(:redirect)
-        expect(response).to redirect_to(public_person_path(person))
+        # User from different organization is redirected to organizations_path by ensure_teammate_matches_organization
+        expect(response).to redirect_to(organizations_path)
       end
     end
   end
