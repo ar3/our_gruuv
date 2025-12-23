6313a9f71a62fee349a2f523fceea689b5a15632
AR3 <ar3@ar3.me>
2025-12-22 23:24:48 -0500

    Clean up observation large list

--- app/helpers/observations_helper.rb
+++ app/helpers/observations_helper.rb
@@ -126,5 +126,61 @@ module ObservationsHelper
       'Observation'
     end
   end
+
+  def privacy_level_display_text(observation)
+    observee_names = observation.observed_teammates.map { |t| t.person.casual_name }
+    
+    # Get manager names for all observees (deduplicated)
+    manager_names = []
+    observation.observed_teammates.each do |teammate|
+      managers = ManagerialHierarchyQuery.new(person: teammate.person, organization: observation.company).call
+      managers.each do |manager_info|
+        manager_name = manager_info[:name]
+        manager_names << manager_name unless manager_names.include?(manager_name)
+      end
+    end
+    
+    case observation.privacy_level
+    when 'observer_only'
+      '🔒 Just for me (Journal)'
+    when 'observed_only'
+      if observee_names.any?
+        "👤 Just Observees: #{observee_names.join(', ')}"
+      else
+        '👤 Just for their eyes only'
+      end
+    when 'managers_only'
+      if manager_names.any?
+        "👔 Just Managers: #{manager_names.join(', ')}"
+      else
+        '👔 Just for their manager\'s eyes only'
+      end
+    when 'observed_and_managers'
+      observee_text = observee_names.any? ? "Observees: #{observee_names.join(', ')}" : 'Observees'
+      manager_text = manager_names.any? ? "Managers: #{manager_names.join(', ')}" : 'Managers'
+      "👥 #{observee_text} AND #{manager_text}"
+    when 'public_to_company'
+      "🏢 Public to #{observation.company.name}"
+    when 'public_to_world'
+      '🌍 Public to world'
+    else
+      observation.privacy_level.humanize
+    end
+  end
+
+  def observation_date_text(observation)
+    created_date = observation.created_at.to_date
+    published_date = observation.published_at&.to_date
+    
+    if observation.draft?
+      "draft created on #{observation.created_at.strftime('%m/%d/%y')}"
+    elsif published_date && created_date == published_date
+      "published on #{observation.published_at.strftime('%m/%d/%y')}"
+    elsif published_date
+      "created on #{observation.created_at.strftime('%m/%d/%y')} and published on #{observation.published_at.strftime('%m/%d/%y')}"
+    else
+      "created on #{observation.created_at.strftime('%m/%d/%y')}"
+    end
+  end
 end
 
