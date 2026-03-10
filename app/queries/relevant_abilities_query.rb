4b8542913e81d4d08260aa6c291804763d83d00d
AR3 <ar3@ar3.me>
2026-03-10 06:52:19 -0400

    N+1 fix: Check-ins Page : Abilities

--- app/queries/relevant_abilities_query.rb
+++ app/queries/relevant_abilities_query.rb
@@ -67,45 +67,60 @@ class RelevantAbilitiesQuery
 
   def build_ability_data(relevant_ability_ids)
     abilities = load_abilities_with_associations(relevant_ability_ids)
+    ability_ids = abilities.map(&:id)
+
+    # Batch load to avoid N+1: teammate_milestones, assignment_requirements, position_requirements per ability
+    milestones_by_ability_id = preload_milestone_attainments_by_ability(ability_ids)
+    assignment_requirements_by_ability_id = preload_assignment_requirements_by_ability(ability_ids)
+    position_requirements_by_ability_id = preload_position_requirements_by_ability(ability_ids)
 
     abilities.map do |ability|
       {
         ability: ability,
-        milestone_attainments: milestone_attainments_for(ability),
-        assignment_requirements: assignment_requirements_for(ability),
-        position_requirements: position_requirements_for(ability)
+        milestone_attainments: milestones_by_ability_id[ability.id] || [],
+        assignment_requirements: assignment_requirements_by_ability_id[ability.id] || [],
+        position_requirements: position_requirements_by_ability_id[ability.id] || []
       }
     end
   end
 
   def load_abilities_with_associations(ability_ids)
     Ability.where(id: ability_ids)
-           .includes(:teammate_milestones, assignment_abilities: [:assignment], position_abilities: [:position])
+           .includes(assignment_abilities: [:assignment], position_abilities: [:position])
            .order(:name)
   end
 
-  def milestone_attainments_for(ability)
-    @teammate.teammate_milestones
-              .where(ability: ability)
-              .by_milestone_level
-              .includes(:ability, certifying_teammate: :person)
+  def preload_milestone_attainments_by_ability(ability_ids)
+    return {} if ability_ids.empty?
+
+    TeammateMilestone
+      .where(teammate_id: @teammate.id, ability_id: ability_ids)
+      .by_milestone_level
+      .includes(:ability, certifying_teammate: :person)
+      .group_by(&:ability_id)
   end
 
-  def assignment_requirements_for(ability)
+  def preload_assignment_requirements_by_ability(ability_ids)
+    return {} if ability_ids.empty?
+
     AssignmentAbility
       .joins(assignment: :assignment_tenures)
       .where(assignment_tenures: { teammate_id: @teammate.id, ended_at: nil })
-      .where(ability: ability)
+      .where(ability_id: ability_ids)
       .includes(assignment: :assignment_tenures)
+      .group_by(&:ability_id)
   end
 
-  def position_requirements_for(ability)
+  def preload_position_requirements_by_ability(ability_ids)
     active_tenure = @teammate.active_employment_tenure
-    return [] unless active_tenure&.position
+    return {} unless active_tenure&.position
 
-    active_tenure.position.position_abilities
-      .where(ability: ability)
+    active_tenure
+      .position
+      .position_abilities
+      .where(ability_id: ability_ids)
       .by_milestone_level
+      .group_by(&:ability_id)
   end
 end
 
