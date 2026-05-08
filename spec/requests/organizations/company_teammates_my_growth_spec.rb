6d2353cea265d67f24fb2a859de5840635f22f39
AR3 <ar3@ar3.me>
2026-05-08 06:18:35 -0400

    All goals can now have a check-in

--- spec/requests/organizations/company_teammates_my_growth_spec.rb
+++ spec/requests/organizations/company_teammates_my_growth_spec.rb
@@ -240,8 +240,9 @@ RSpec.describe 'Company teammate My Growth', type: :request do
       it 'allows GET my_growth/goals' do
         get my_growth_goals_organization_company_teammate_path(organization, employee_teammate)
         expect(response).to have_http_status(:success)
-        expect(response.body).to include("Go review #{employee.casual_name}'s")
+        expect(response.body).to include('draft + active')
         expect(response.body).to include('Go check-in on')
+        expect(response.body).to include('draft goals')
       end
 
       it 'allows GET my_growth/position_change' do
